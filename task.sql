a1532b8024efc1d608eee72ba2918c9332d754d8
ladmael <don.sosiska339@gmail.com>
2024-02-01 23:35:53 +0200

    resolved

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-CREATE INDEX email ON Customers (Email); 
+CREATE INDEX Email ON Customers (Email); 
 
-CREATE INDEX products ON Products (Name);
+CREATE INDEX Name ON Products (Name);
