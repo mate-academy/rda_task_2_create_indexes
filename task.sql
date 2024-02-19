74356d42042eee334ff10d69f64f513aa4a08453
Khrystyna Yelyseyeva <play1409@gmail.com>
2024-02-19 15:17:17 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,4 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+CREATE INDEX idx_name ON Products(Name);
+CREATE INDEX idx_warehouseAmount ON Products(WarehouseAmount);
+CREATE INDEX idx_email ON Customers(Email);
+CREATE INDEX idx_date ON Orders(Date);
