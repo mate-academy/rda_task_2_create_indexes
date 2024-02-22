53b6ab70d868e1ae66bb81c47e53430ac35b266a
Khrystyna Yelyseieva <42446716+KhrystynaYelyseyeva@users.noreply.github.com>
2024-02-22 18:28:01 +0200

    Solution2

--- task.sql
+++ task.sql
@@ -1,6 +1,4 @@
 USE ShopDB;
 
-CREATE INDEX idx_name ON Products(Name);
-CREATE INDEX idx_warehouseAmount ON Products(WarehouseAmount);
-CREATE INDEX idx_email ON Customers(Email);
-CREATE INDEX idx_date ON Orders(Date);
+CREATE INDEX Name ON Products (Name);
+CREATE INDEX Email ON Customers (Email);
