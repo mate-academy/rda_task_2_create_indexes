d4eb6906ae9d6d06546bb5c6a6fcc1fd2add05e5
D.Spyrydonov <dakurlzz1543@gmail.com>
2025-05-30 16:43:21 +0300

    fix

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB;
 
-create index idx_address on Customers (Address);
+create index idx_email on Customers (Email);
 create index idx_product on Products (Name);
