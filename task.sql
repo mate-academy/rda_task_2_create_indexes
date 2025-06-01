2a55b8a6b706983ca16cc7a58f855e172be75575
Yevgene-DP <ebalalay@gmail.com>
2025-06-01 22:18:11 +0300

    Add indexes for performance optimization based on usage scenarios

--- task.sql
+++ task.sql
@@ -1,2 +1,12 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+-- Індекс для швидкого пошуку користувачів за Email при вході
+CREATE INDEX Email ON Customers(Email);
+
+-- Індекс для прискорення пошуку продуктів за Name
+CREATE INDEX Name ON Products(Name);
+
+-- Індекс для прискорення вставки та зв'язування замовлень із клієнтами
+CREATE INDEX CustomerID ON Orders(CustomerID);
+
+-- Індекси для оптимізації зв'язків між OrderItems і Orders / Products
+CREATE INDEX OrderID ON OrderItems(OrderID);
+CREATE INDEX ProductID ON OrderItems(ProductID);
