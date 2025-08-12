bdae0676f4ef3e4ea79d86c885fb328cf676d2b8
Tim <tymur.prokhorov@asquad.io>
2025-08-12 17:56:27 +0300

    minor fixes

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
-CREATE INDEX idx_email ON Customers (Email);
-CREATE INDEX idx_name ON Products (Name);
+CREATE INDEX IF NOT EXISTS Email ON Customers (Email);
+CREATE INDEX IF NOT EXISTS Name ON Products (Name);
