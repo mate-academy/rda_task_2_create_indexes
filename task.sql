b0357aa216d0ad2ba682fa49ea378419e3b09d06
myserver <ihorkondrashov@gmail.com>
2025-08-15 13:55:18 +0000

    indexes

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 USE ShopDB; 
-CREATE INDEX idx_email ON Customers (Email); 
-CREATE INDEX idx_prodname ON Products (Name); 
-CREATE INDEX ProductID ON OrderItems (ProductID);
-CREATE INDEX OrderID ON OrderItems (OrderID);
-CREATE INDEX CustomerID ON Orders (CustomerID);
+CREATE INDEX idx_customers_email ON Customers(Email); 
+CREATE INDEX idx_product_name ON Products(Name); 
+CREATE INDEX ProductID ON OrderItems(ProductID);
+CREATE INDEX OrderID ON OrderItems(OrderID);
+CREATE INDEX CustomerID ON Orders(CustomerID);
