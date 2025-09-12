165b183b775fbcd8c10ce7e535953e9911d60265
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-12 13:56:14 +0300

    Solution6

--- task.sql
+++ task.sql
@@ -5,7 +5,5 @@ CREATE INDEX Name ON Products (Name);
 
 CREATE UNIQUE INDEX Email ON Customers (Email);
 
-CREATE INDEX idx_orders_customer ON Orders (CustomerID);
-CREATE INDEX idx_orders_date ON Orders (Date);
 
 
