15d53638da16f8ecccc28e720261d42013a0dbba
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-12 13:41:11 +0300

    Solution1

--- task.sql
+++ task.sql
@@ -1,46 +1,14 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB;
 
-CREATE TABLE Products (
-    ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Description VARCHAR(100),
-    Price DECIMAL(10,2),
-    WarehouseAmount INT,
-    PRIMARY KEY (ID)
-);
+CREATE INDEX idx_products_name ON Products (Name);
 
-ALTER TABLE Products ADD INDEX idx_products_name (Name);
 
-CREATE TABLE Customers (
-    ID INT AUTO_INCREMENT,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Email VARCHAR(100),
-    Address VARCHAR(100),
-    PRIMARY KEY (ID),
-    UNIQUE INDEX idx_customers_email (Email)
-);
+CREATE INDEX idx_orders_customer ON Orders (CustomerID);
+CREATE INDEX idx_orders_date ON Orders (Date);
 
-CREATE TABLE Orders (
-    ID INT AUTO_INCREMENT,
-    CustomerID INT NULL,
-    Date DATE,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
-);
 
-ALTER TABLE Orders ADD INDEX idx_orders_customer (CustomerID);
-ALTER TABLE Orders ADD INDEX idx_orders_date (Date);
 
-CREATE TABLE OrderItems (
-    ID INT AUTO_INCREMENT,
-    OrderID INT NULL,
-    ProductID INT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
-);
+CREATE INDEX idx_orderitems_order ON OrderItems (OrderID);
+CREATE INDEX idx_orderitems_product ON OrderItems (ProductID);
 
-ALTER TABLE OrderItems ADD INDEX idx_orderitems_order (OrderID);
-ALTER TABLE OrderItems ADD INDEX idx_orderitems_product (ProductID);
