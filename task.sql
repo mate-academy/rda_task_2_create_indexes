8b4a58f7fef28fb89eba75fdc8c7f95e2c1c61be
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-12 14:46:34 +0300

    Solition1.0

--- task.sql
+++ task.sql
@@ -3,7 +3,9 @@ USE ShopDB;
 
 CREATE INDEX Name ON Products (Name);
 
-CREATE UNIQUE INDEX Email ON Customers (Email);
+CREATE INDEX Email ON Customers (Email);
 
-
+CREATE INDEX CustomerID ON Orders(CustomerID);
+CREATE INDEX OrderID ON OrderItems(OrderID);
+CREATE INDEX ProductID ON OrderItems(ProductID);
 
