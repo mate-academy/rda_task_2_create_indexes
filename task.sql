12dd1c3b5f49a40b307714ef153c082802816998
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-12 13:44:46 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -3,11 +3,10 @@ USE ShopDB;
 
 CREATE INDEX idx_products_name ON Products (Name);
 
+CREATE UNIQUE INDEX idx_customers_email ON Customers (Email);
 
 CREATE INDEX idx_orders_customer ON Orders (CustomerID);
 CREATE INDEX idx_orders_date ON Orders (Date);
-
-
 
 CREATE INDEX idx_orderitems_order ON OrderItems (OrderID);
 CREATE INDEX idx_orderitems_product ON OrderItems (ProductID);
