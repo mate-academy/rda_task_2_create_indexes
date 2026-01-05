c27c4b98575ac92886238712a3b989df09581bd2
godfri228 <godfri228@users.noreply.github.com>
2026-01-05 20:31:45 +0200

    Create indexes for ShopDB database

--- task.sql
+++ task.sql
@@ -1,2 +1,4 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+CREATE UNIQUE INDEX idx_customers_email ON Customers(Email);
+CREATE INDEX idx_products_price_warehouse ON Products(Price, WarehouseAmount);
+CREATE INDEX idx_orders_date ON Orders(Date);
+CREATE INDEX idx_orderitems_order_product ON OrderItems(OrderID, ProductID);
