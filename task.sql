4d31f3c5a91d1226b6ce0025576d0f25bd27b3bf
Ivan <ivanqqq@ukr.net>
2026-03-11 15:12:41 +0100

    idx's names have changed

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 # Write your SQL code for the database creation here. Good luck!
 USE ShopDB;
 
-CREATE INDEX idx_email ON Customers(Email);
-CREATE INDEX idx_product_name ON Products(Name);
+CREATE INDEX Email ON Customers(Email);
+CREATE INDEX Name ON Products(Name);
