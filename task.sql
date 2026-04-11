d3ae68ce64b492112aa56e6f614f74447ec79ef2
Shershen Oleksandra <o_shershen@viyar.ua>
2026-04-11 13:12:58 +0300

    feat(task.sql): add indexes

--- task.sql
+++ task.sql
@@ -1,2 +1,13 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+-- Customers login by email
+CREATE UNIQUE INDEX Email ON Customers(Email);
+
+-- Product search by name
+CREATE INDEX Name ON Products(Name);
+
+-- Foreign key indexes for fast joins
+CREATE INDEX CustomerID ON Orders(CustomerID);
+
+CREATE INDEX OrderID ON OrderItems(OrderID);
+CREATE INDEX ProductID ON OrderItems(ProductID);
