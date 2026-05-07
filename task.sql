92b452e6356013fcf4b0eb8126bd34a8ddf32be8
Ivan Sotsenko <vanja.sotsenko@gmail.com>
2026-05-07 08:39:22 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,4 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+CREATE INDEX index_email ON Customers (Email);
+CREATE INDEX index_name_product ON Products (Name);
+CREATE INDEX index_price ON Products (Price);
