17b449b7904038a40e72638f417cfd0f4e6c4c78
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-03 12:45:57 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE INDEX user_email_index ON Customers(Email);
+CREATE INDEX product_name_index ON Products(Name);
