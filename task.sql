156ce5233dd6e9be9deb38fce44f0bbe6eb06a88
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-08 10:12:23 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,37 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+CREATE DATABASE ShopDB;
+USE ShopDB;
+
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    Description VARCHAR(100),
+    Price DOUBLE,
+    WarehouseAmount INT
+);
+
+CREATE TABLE Customers (
+    ID INT AUTO_INCREMENT PRIMARY KEY,,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Address VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
+    Date DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+);
+
+CREATE TABLE OrderItems (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+);
+
+CREATE INDEX idx_name ON Products (Name);
+CREATE INDEX idx_price ON Products (Price);
+CREATE INDEX idx_customer ON Customers (Email);
