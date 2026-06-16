3f1be853d9c9fe0b42c25035108f87184316162e
ll221 <ihor.kunakh.kb.2022@lpnu.ua>
2026-06-16 19:49:18 +0300

    2

--- task.sql
+++ task.sql
@@ -1,7 +1,14 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
 -- Логін: пошук клієнта по email
 CREATE INDEX Email ON Customers(Email);
 
 -- Пошук товару по назві
 CREATE INDEX Name ON Products(Name);
+
+-- Пошук замовлень конкретного клієнта (100+ замовлень на день = частий JOIN)
+CREATE INDEX CustomerID ON Orders(CustomerID);
+
+-- Пошук позицій конкретного замовлення
+CREATE INDEX OrderID ON OrderItems(OrderID);
+
+-- Пошук замовлень що містять певний товар
+CREATE INDEX ProductID ON OrderItems(ProductID);
