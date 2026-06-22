a88c5286415b5f945a17926ec857e8ed2cbd95d7
maxmlv <max.mlv.k@gmail.com>
2026-06-22 16:48:58 +0300

    fix: add indexes for Orders and OrderItems

--- task.sql
+++ task.sql
@@ -3,3 +3,7 @@ USE ShopDB;
 CREATE INDEX Email ON Customers (Email);
 
 CREATE INDEX Name ON Products (Name);
+
+CREATE INDEX CustomerID ON Orders (CustomerID);
+
+CREATE INDEX ProductID ON OrderItems (ProductID);
