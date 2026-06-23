8553e0714c64c72b86c2df6ef829c08269406139
1ntact <verbaivan9@gmail.com>
2026-06-23 19:27:36 +0200

    Done!!

--- task.sql
+++ task.sql
@@ -1,4 +1,7 @@
 USE ShopDB;
 
+CREATE INDEX Name ON Products (Name);
 CREATE INDEX Email ON Customers (Email);
 CREATE INDEX CustomerID ON Orders (CustomerID);
+CREATE INDEX OrderID ON OrderItems (OrderID);
+CREATE INDEX ProductID ON OrderItems (ProductID);
