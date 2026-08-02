2f5d1537af36b234d563d66bee15de9e00531285
Diana Khmeliuk <dianakhmeluyk@gmail.com>
2026-08-02 10:23:12 +0300

    'Solution'

--- task.sql
+++ task.sql
@@ -1,2 +1,7 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE UNIQUE INDEX idx_customer_email ON Customers(Email);
+CREATE INDEX idx_products_name ON Products(Name);
+CREATE INDEX idx_customer_orders ON Orders(CustomerID);
+CREATE INDEX idx_items_order ON OrderItems(OrderID);
