b8b8d336b12d65f218be64ec00a4a5909eeb72ce
Volodymyr Mulyko <vovamulyka@gmail.com>
2026-08-03 23:12:40 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,4 +1,7 @@
-USE ShopDB; 
+USE ShopDB;
 
 CREATE INDEX Email ON Customers (Email);
 CREATE INDEX Name ON Products (Name);
+CREATE INDEX CustomerID ON Orders (CustomerID);
+CREATE INDEX OrderID ON OrderItems (OrderID);
+CREATE INDEX ProductID ON OrderItems (ProductID);
