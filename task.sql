8ba011318c9cf0718081b602e8a1526492bcbba7
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-08-31 18:38:14 +0300

    Update commits for Products and Customers table

--- task.sql
+++ task.sql
@@ -1,5 +1,9 @@
 USE ShopDB;
 
+CREATE INDEX ProductID ON Products (ID);
+
 CREATE INDEX Name ON Products (Name);
+
+CREATE INDEX CustomerID ON Customers (ID);
 
 CREATE INDEX Email ON Customers (Email);
