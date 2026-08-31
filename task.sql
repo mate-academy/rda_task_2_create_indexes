087a750aef90041dd59c7c3c5b6215c44406bed2
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-08-31 19:01:42 +0300

    Update 2

--- task.sql
+++ task.sql
@@ -1,9 +1,13 @@
 USE ShopDB;
 
-CREATE INDEX ProductID ON Products (ID);
-
 CREATE INDEX Name ON Products (Name);
 
-CREATE INDEX CustomerID ON Customers (ID);
-
 CREATE INDEX Email ON Customers (Email);
+
+CREATE INDEX idx_orders_customer_id ON Orders (CustomerID);
+
+CREATE INDEX idx_orders_date ON Orders (Date);
+
+CREATE INDEX idx_order_items_order_id ON OrderItems (OrderID);
+
+CREATE INDEX idx_order_items_product_id ON OrderItems (ProductId);
