cb019ad51228c98e001b303ba8508358b6da0ad0
Vadym <vadym@example.com>
2026-09-11 09:54:39 +0300

    Solution3

--- task.sql
+++ task.sql
@@ -1,3 +1,3 @@
 USE ShopDB;
-CREATE INDEX idx_customers_email ON Customers (Email);
-CREATE INDEX idx_products_name ON Products (Name);
+CREATE INDEX Email ON Customers (Email);
+CREATE INDEX Name ON Products (Name);
